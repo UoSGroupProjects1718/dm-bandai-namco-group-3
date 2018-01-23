152e1a00b383aa23507ff142b4d07bf2edd4d9ca
Ben Reynolds <breynolds1994@outlook.com>
2018-01-23 21:36:06 +0000

    Implement DraggableObject class

---
+++ Prototypes/Physics Game/Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableObject : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private bool _draggable;
+    
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (_draggable)
+        {
+            var currentZ = _transform.position.z;
+            _transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            _transform.localPosition = new Vector3(_transform.localPosition.x,
+                _transform.localPosition.y, currentZ);
+        }
+    }
+
+    public void setDraggable(bool draggable)
+    {
+        _draggable = draggable;
+    }
+
+    public bool isDraggable()
+    {
+        return _draggable;
+    }
+}

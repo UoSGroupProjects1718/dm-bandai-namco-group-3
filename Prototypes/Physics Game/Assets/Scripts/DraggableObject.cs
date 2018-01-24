1d8580fe94694d5fc2c2c219a5c58c6dafc53bb1
Ben Reynolds <breynolds1994@outlook.com>
2018-01-24 02:44:24 +0000

    Begin implementing classes and scene structure

--- Prototypes/Physics Game/Assets/Scripts/DraggableObject.cs
+++ Prototypes/Physics Game/Assets/Scripts/DraggableObject.cs
@@ -3,33 +3,30 @@ using UnityEngine.EventSystems;
 
 public class DraggableObject : MonoBehaviour, IDragHandler
 {
-    [SerializeField] private bool _draggable;
+    public bool Draggable;
     
-    private Transform _transform;
-
-    private void Start()
-    {
-        _transform = GetComponent<Transform>();
-    }
-    
+    [SerializeField] private Collider2D _physicalCollider;
+    [SerializeField] private LayerMask _outOfBoundsLayerMask;
+        
     public void OnDrag(PointerEventData eventData)
     {
-        if (_draggable)
+        // If the object is not flagged as draggable then do nothing.
+        if (!Draggable) return;
+        
+        // Obtain the position of the touch in world coordinates and calculate the object's potential next position. 
+        var touchPostion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        var nextPosition = new Vector3(touchPostion.x, touchPostion.y, transform.position.z);
+
+        // Calculate the distance and direction of the desired movement.
+        var heading = nextPosition - transform.position;
+        var distance = heading.magnitude + _physicalCollider.bounds.extents.magnitude;
+        var direction = heading / distance;
+
+        // Cast a ray in the movement direction to detect if it is out of bounds. If it isn't, move the object.
+        Debug.DrawLine(transform.position, transform.position + (direction * distance));
+        if(!Physics2D.Raycast(transform.position, direction, distance, _outOfBoundsLayerMask))
         {
-            var currentZ = _transform.position.z;
-            _transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            _transform.localPosition = new Vector3(_transform.localPosition.x,
-                _transform.localPosition.y, currentZ);
-        }
-    }
-
-    public void setDraggable(bool draggable)
-    {
-        _draggable = draggable;
-    }
-
-    public bool isDraggable()
-    {
-        return _draggable;
+            transform.position = nextPosition;
+        }       
     }
 }
---
+++ Prototypes/Physics Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team
+{
+	Red, Green
+}
+
+public class GameManager : MonoBehaviour {
+
+	private static readonly Color TeamRedColour = new Color(255.0f / 255.0f, 105.0f / 255.0f, 97.0f / 255.0f);
+	private static readonly Color TeamGreenColour = new Color(119.0f / 255.0f, 211.0f / 255.0f, 119.0f / 255.0f);
+
+	private Team _activeTeam;
+
+	[SerializeField] private Fish _fish;
+	
+	private static GameManager _instance;
+	
+	public static GameManager Instance => _instance ?? (_instance = FindObjectOfType<GameManager>());
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		SetupGame();
+	}
+
+	private void SetupGame()
+	{
+		Debug.Log("Setting up game...");
+		_activeTeam = Random.Range(0, 2) == 0 ? Team.Red : Team.Green;
+		Debug.LogFormat("Team {0} starts...", _activeTeam);
+		_fish.SetColour(_activeTeam == Team.Red ? TeamRedColour : TeamGreenColour);
+		_fish.SetPhysicsEnabled(true);
+	}
+	
+}

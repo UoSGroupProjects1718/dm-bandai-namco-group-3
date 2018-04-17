8b2516a452680a0ff70d755e399ccca2ee70e234
Ben Reynolds <breynolds1994@outlook.com>
2018-04-17 12:13:40 +0100

    Implement feature that keeps screen alive during gameplay

---
+++ Medieval Mayhem/Assets/Scripts/KeepScreenAlive.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class KeepScreenAlive : MonoBehaviour {
+    
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+    
+}
